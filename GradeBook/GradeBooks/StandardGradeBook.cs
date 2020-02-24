ff0ed0ad007a12ff3f05407f605df5d46a7f5390
samart <samart@TUCC-CEC260>
2020-02-24 13:54:06 -0600

    Test 2

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -10,11 +10,7 @@ namespace GradeBook.GradeBooks
         public StandardGradeBook(string name): base(name)
         {
             Type = Enums.GradeBookType.Standard;
-            fdfd
-
-            rtrtr
-
-            rerfer
+            
         }
     }
 }
