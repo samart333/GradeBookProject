fc543d59f982c8c5f50894898d4d9be50245b0e5
samart <samart@TUCC-CEC260>
2020-02-24 13:48:18 -0600

    Add files

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook: BaseGradeBook
+    {
+        public StandardGradeBook(string name): base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+            fdfd
+
+            rtrtr
+        }
+    }
+}
